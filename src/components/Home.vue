6f05cb2f1ddd5ff091f44d20b62d555ed6ca816e
Matt Carbonaro <mcarbona24@gmail.com>
2025-05-21 22:12:10 -0600

    initial vue setup

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <h1>Hello, my name is Matt Carbonaro</h1>
+  <h4>I am a professional backend engineer for Lockheed Martin and a hobbiest frontend developer.</h4>
+</template>
